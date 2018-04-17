92221b839e4b18796647f47932bb59128ff94b43
Alexandre Chêne <kooparse@protonmail.com>
2018-04-17 14:35:57 +0200

    That's one small step for a man

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Renderer render = gameObject.GetComponent<Renderer>();
+		render.material.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# project_one
+[Classified] Top Secret
